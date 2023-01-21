cb9bea560c6600f4d00a5c56a7a8dfddc41cef24
Kiwifuit <mahkiwi123@gmail.com>
2023-01-21 21:48:10 +0800

    Opted to log to a file instead of stdout

--- src/logger.rs
+++ src/logger.rs
@@ -1,35 +1,33 @@
 pub use log::Level;
-use std::io;
-
-use fern_colored::{
-    colors::{Color, ColoredLevelConfig},
-    Dispatch,
-};
+use std::path::PathBuf;
 
 use chrono::Local;
+use fern_colored::{log_file, Dispatch};
 
 pub fn setup_logger(level: Level) -> Result<(), String> {
-    let colors = ColoredLevelConfig::new()
-        .info(Color::TrueColor { r: 0, g: 0, b: 0 })
-        .debug(Color::TrueColor { r: 0, g: 0, b: 0 })
-        .trace(Color::TrueColor { r: 0, g: 0, b: 0 })
-        .warn(Color::TrueColor { r: 0, g: 0, b: 0 })
-        .error(Color::TrueColor { r: 0, g: 0, b: 0 });
-
     Dispatch::new()
         .format(move |out, msg, record| {
             out.finish(format_args!(
                 "[{}] [{}] [{}] {}",
                 Local::now().format("%H:%M:%S"),
                 record.module_path().or(Some("<module>")).unwrap(),
-                colors.color(record.level()),
+                record.level(),
                 msg
             ))
         })
-        .chain(io::stdout())
+        .chain(
+            log_file(get_log_file())
+                .map_err(|e| format!("An error occurred while trying to open file: {}", e))?,
+        )
         .level(level.to_level_filter())
         .apply()
         .map_err(|e| format!("An error occurred while trying to set up the logger: {}", e))?;
 
     Ok(())
+}
+
+fn get_log_file() -> PathBuf {
+    let filename = Local::now().format("%d%m%y-%H%M%S.log");
+
+    PathBuf::from(filename.to_string())
 }

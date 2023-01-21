fbec0cbea0bb3803d5b5a195342280b3db1bcc00
Kiwifuit <mahkiwi123@gmail.com>
2023-01-21 19:54:49 +0800

    Exposed `log::Level` and changed formatting

--- src/logger.rs
+++ src/logger.rs
@@ -1,3 +1,4 @@
+pub use log::Level;
 use std::io;
 
 use fern_colored::{
@@ -7,7 +8,7 @@ use fern_colored::{
 
 use chrono::Local;
 
-pub fn setup_logger() -> Result<(), String> {
+pub fn setup_logger(level: Level) -> Result<(), String> {
     let colors = ColoredLevelConfig::new()
         .info(Color::TrueColor { r: 0, g: 0, b: 0 })
         .debug(Color::TrueColor { r: 0, g: 0, b: 0 })
@@ -18,14 +19,15 @@ pub fn setup_logger() -> Result<(), String> {
     Dispatch::new()
         .format(move |out, msg, record| {
             out.finish(format_args!(
-                "[{}] [{}] ({}) {}",
-                Local::now(),
-                colors.color(record.level()),
+                "[{}] [{}] [{}] {}",
+                Local::now().format("%H:%M:%S"),
                 record.module_path().or(Some("<module>")).unwrap(),
+                colors.color(record.level()),
                 msg
             ))
         })
         .chain(io::stdout())
+        .level(level.to_level_filter())
         .apply()
         .map_err(|e| format!("An error occurred while trying to set up the logger: {}", e))?;
 

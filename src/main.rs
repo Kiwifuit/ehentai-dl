1d124d6d93ee3dae2e37cefd5a23f696c659972d
Kiwifuit <mahkiwi123@gmail.com>
2023-01-22 14:23:48 +0800

    Post-Overhaul

--- src/main.rs
+++ src/main.rs
@@ -9,42 +9,4 @@ fn main() {
         eprintln!("{msg}");
         exit(1)
     }
-
-    let galleries = gallery::make_galleries("res/galleries.txt").unwrap();
-    let mut threads = vec![];
-    for gallery in galleries {
-        threads.push(spawn(move || {
-            if let Ok(total) = gallery.download_images() {
-                let total = total
-                    .to_string()
-                    .as_bytes()
-                    .rchunks(3)
-                    .rev()
-                    .map(std::str::from_utf8)
-                    .collect::<Result<Vec<&str>, _>>()
-                    .unwrap()
-                    .join(",");
-
-                println!("Downloaded {:} bytes", total);
-            }
-        }));
-    }
-
-    for thread in threads {
-        thread        .join()
-        .unwrap_or_else(|e| {
-            let exit_code;
-
-            if let Ok(res) = e.downcast::<String>() {
-                eprintln!("An error occurred while downloading: {}", res);
-                exit_code = 2;
-            } else {
-                eprintln!("An unknown error occurred while downloading");
-                exit_code = -500;
-            }
-
-            eprintln!("Please check the logs for more information. If this is a development build, please set the logger to DEBUG or TRACE");
-            exit(exit_code)
-        });
-    }
 }

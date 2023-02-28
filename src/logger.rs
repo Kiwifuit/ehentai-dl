e6c6d2e2c5446e332ff7e2b9e1a692b0c9276af0
Kiwifuit <mahkiwi123@gmail.com>
2023-03-01 00:21:20 +0800

    Added Log Levels

--- src/logger.rs
+++ src/logger.rs
@@ -1,10 +1,33 @@
-pub use log::Level;
+use std::io::stdout;
 use std::path::PathBuf;
 
 use chrono::Local;
 use fern_colored::{log_file, Dispatch};
+use log::LevelFilter;
 
-pub fn setup_logger(level: Level) -> Result<(), String> {
+pub enum LogLevel {
+    Off,
+    Trace,
+    Debug,
+    Info,
+    Warn,
+    Error,
+}
+
+impl Into<LevelFilter> for LogLevel {
+    fn into(self) -> LevelFilter {
+        match self {
+            Self::Off => LevelFilter::Off,
+            Self::Trace => LevelFilter::Trace,
+            Self::Debug => LevelFilter::Debug,
+            Self::Info => LevelFilter::Info,
+            Self::Warn => LevelFilter::Warn,
+            Self::Error => LevelFilter::Error,
+        }
+    }
+}
+
+pub fn setup_logger(level: LogLevel) -> Result<(), String> {
     Dispatch::new()
         .format(move |out, msg, record| {
             out.finish(format_args!(
@@ -15,11 +38,12 @@ pub fn setup_logger(level: Level) -> Result<(), String> {
                 msg
             ))
         })
+        .chain(stdout())
         .chain(
             log_file(get_log_file())
                 .map_err(|e| format!("An error occurred while trying to open file: {}", e))?,
         )
-        .level(level.to_level_filter())
+        .level(level.into())
         .apply()
         .map_err(|e| format!("An error occurred while trying to set up the logger: {}", e))?;
 

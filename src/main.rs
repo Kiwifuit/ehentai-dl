b24628d1be0ae6f3260ecb68aacc4d058387d3f1
Kiwifuit <mahkiwi123@gmail.com>
2023-04-01 23:15:19 +0800

    Improved reading system

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use std::{path::Path, sync::mpsc::channel, thread};
+use log::info;
 
 mod extractor;
 mod logger;
 mod parser;
 
-const CHUNK_SIZE: usize = 2048;
+const CHUNK_SIZE: usize = 1024;
 
 fn main() {
     logger::setup_logger(logger::LogLevel::Debug)
         .expect("unexpected error whie starting the logger");
 
-    let a = parser::read_file::<CHUNK_SIZE, str>("res/galleries.txt");
+    let a = parser::read_file::<CHUNK_SIZE, str>("res/galleries.txt").unwrap();
+    let g = parser::get_all_galleries(&a).unwrap();
 
-    dbg!(a);
+    info!("{} galleries to download", g.len());
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,37 +1,34 @@
-use log::{debug, info};
+use log::debug;
 use regex::Regex;
 use std::ffi::OsStr;
 use std::fs::OpenOptions;
 use std::io::{self, prelude::*};
 use std::path::Path;
-use std::sync::mpsc::{self, Sender};
+use std::sync::mpsc::{self, SyncSender};
 use std::{string, thread};
 
 const GALLERY_PARSER: &str = r"https://e-hentai\.org/g/(\d{7})/([a-z0-9]{10})";
 
 #[derive(Debug)]
-/// Wraps various errors into one. `chunk_size` is generally used for
+/// Wraps various errors into one. `C` is generally used for
 /// wrapping `std::mpsc::SendError`, and a value of `0` automatically
 /// signifies that the function does not do any I/O operations
 /// *(technically it means that it wont be wrapping `std::mpsc::SendError`,
 /// and therefore, no I/O)*
-pub enum ParseError<const chunk_size: usize> {
+pub enum ParseError<const C: usize> {
     IoError(io::Error),
-    ChannelError(mpsc::SendError<([u8; chunk_size], usize)>),
+    ChannelError(mpsc::SendError<([u8; C], usize)>),
     RegexParseError(regex::Error),
     StringEncodeError(string::FromUtf8Error),
 }
 
 /// Loads a file chunk by chunk and sends it via `tx`, along with how many
 /// bytes it has read.
-///
-/// Right before the function finishes, it sends an empty buffer, along
-/// with how many bytes it has read total
-fn load_file<const chunk_size: usize>(
+fn load_file<const CHUNK_SIZE: usize>(
     path: &Path,
-    tx: Sender<([u8; chunk_size], usize)>,
-) -> Result<usize, ParseError<chunk_size>> {
-    let mut buf = [0; chunk_size];
+    tx: SyncSender<([u8; CHUNK_SIZE], usize)>,
+) -> Result<usize, ParseError<CHUNK_SIZE>> {
+    let mut buf = [0; CHUNK_SIZE];
 
     let mut file = OpenOptions::new()
         .read(true)
@@ -47,37 +44,31 @@ fn load_file<const chunk_size: usize>(
 
         buf.fill(0);
     }
-    tx.send((buf, bytes_read_total))
-        .map_err(|e| ParseError::ChannelError(e))?;
 
     Ok(bytes_read_total)
 }
 
-pub fn read_file<const chunk_size: usize, F>(file: &'static F) -> Result<String, ParseError<0>>
+pub fn read_file<const CHUNK_SIZE: usize, F>(file: &'static F) -> Result<String, ParseError<0>>
 where
     F: AsRef<OsStr> + ?Sized,
 {
     let file = Path::new(file);
-    let (file_tx, file_rx) = mpsc::channel();
+    let (file_tx, file_rx) = mpsc::sync_channel(100);
 
-    thread::spawn(move || load_file::<chunk_size>(&file, file_tx));
+    thread::spawn(move || load_file::<CHUNK_SIZE>(&file, file_tx));
 
     let mut res = String::new();
-    while let Ok(payload) = file_rx.recv() {
-        match payload {
-            // TODO: Log how many bytes were read
-            (chunk, read_total) if chunk.iter().map(|i| *i as usize).sum::<usize>() == 0 => {
-                info!("Read {} bytes", read_total)
-            }
-            (chunk, read_total) => {
-                let bytes = chunk
-                    .iter()
-                    .take(read_total)
-                    .map(|i| *i)
-                    .collect::<Vec<u8>>();
-                res += &String::from_utf8(bytes).map_err(|e| ParseError::StringEncodeError(e))?;
-            }
+    while let Ok((chunk, bytes_read)) = file_rx.recv() {
+        if bytes_read == 0 {
+            break;
         }
+        let bytes = chunk
+            .iter()
+            .take(bytes_read)
+            .map(|i| *i)
+            .collect::<Vec<u8>>();
+        debug!("Read {}/{} bytes", bytes_read, CHUNK_SIZE);
+        res += &String::from_utf8(bytes).map_err(|e| ParseError::StringEncodeError(e))?;
     }
 
     Ok(res)

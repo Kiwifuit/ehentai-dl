96a29b7d27155758cb7a9f8576380b64634966ef
Kiwifuit <mahkiwi123@gmail.com>
2023-05-20 22:22:18 +0800

    Added git hash to version

--- src/version.rs
+++ src/version.rs
@@ -1,13 +1,16 @@
 const PROGRAM_NAME: &str = env!("CARGO_PKG_NAME");
 const PROGRAM_VERSION: &str = env!("CARGO_PKG_VERSION");
+const PROGRAM_COMMIT: &str = include_str!("../.git/refs/heads/master");
+const COMMIT_HASH_LENGTH: usize = 8;
 
 pub fn get_version() -> String {
     let features = get_features();
 
     format!(
-        "{} v{} {}",
+        "{} v{}@{} {}",
         PROGRAM_NAME,
         PROGRAM_VERSION,
+        &PROGRAM_COMMIT[..COMMIT_HASH_LENGTH],
         if !features.is_empty() {
             format!("w/{}", features.join(","))
         } else {

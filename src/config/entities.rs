618e6eecee07da381a395d446c93f20341780d72
Kiwifuit <mahkiwi123@gmail.com>
2023-06-02 14:01:41 +0800

    Simplified config

--- src/config/entities.rs
+++ src/config/entities.rs
@@ -4,12 +4,5 @@ use serde::Deserialize;
 #[cfg(feature = "config")]
 #[derive(Deserialize)]
 pub struct Config {
-    config: ConfigKeys,
-}
-
-#[cfg(feature = "config")]
-#[derive(Deserialize)]
-pub(super) struct ConfigKeys {
     features: Vec<String>,
-    chunk_size: usize,
 }
